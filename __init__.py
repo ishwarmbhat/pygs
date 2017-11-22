772a904e13414e83474cfdfe486053ac1b6c92fc
Ishwar M <ishwarm.bhat@gmail.com>
2017-11-22 12:36:28 +0530

    __init__.py modules added to it

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""pygs - Library dedicated to Geospatial computations. Individual functions can be understood using their docstrings
+Modules include:
+gslinalg.py - Computations related to regression and correlation for 3D datasets (Spatio temporal datasets)
+gsncl.py - Porting NCL like functions to Python like area averaging, area sum and volume computation
+gsplots.py - Simplifying geospatial plotting and hovmoller diagrams. Other functions will be added as and when required
+statmisc.py - Small modele with statistical functions related to significance level calculations, time series, and so on..."""
+
+import gsplots
+import gsncl
+import statmisc
+import gslinalg
